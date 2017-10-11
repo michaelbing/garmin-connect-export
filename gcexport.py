e32472bf8c96293aac264602c1856b1e4f300bef
Michael Bing <michaelbing@outlook.dk>
2017-10-11 15:49:29 +0200

    Upgrade to Python 3 and fix various issues

--- gcexport.py
+++ gcexport.py
@@ -9,7 +9,7 @@ Description:	Use this script to export your fitness data from Garmin Connect.
 				See README.md for more information.
 """
 
-from urllib import urlencode
+from urllib.parse import urlencode
 from datetime import datetime
 from getpass import getpass
 from sys import argv
@@ -19,7 +19,7 @@ from os import mkdir
 from os import remove
 from xml.dom.minidom import parseString
 
-import urllib2, cookielib, json
+import urllib.request, urllib.error, urllib.parse, http.cookiejar, json
 from fileinput import filename
 
 import argparse
@@ -53,20 +53,20 @@ parser.add_argument('-u', '--unzip',
 args = parser.parse_args()
 
 if args.version:
-	print argv[0] + ", version " + script_version
+	print(argv[0] + ", version " + script_version)
 	exit(0)
 
-cookie_jar = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
+cookie_jar = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie_jar))
 
 # url is a string, post is a dictionary of POST parameters, headers is a dictionary of headers.
 def http_req(url, post=None, headers={}):
-	request = urllib2.Request(url)
+	request = urllib.request.Request(url)
 	request.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/1337 Safari/537.36')  # Tell Garmin we're some supported browser.
-	for header_key, header_value in headers.iteritems():
+	for header_key, header_value in headers.items():
 		request.add_header(header_key, header_value)
 	if post:
-		post = urlencode(post)  # Convert dictionary to POST parameter string.
+		post = urlencode(post).encode('utf-8')  # Convert dictionary to POST parameter string.
 	response = opener.open(request, data=post)  # This line may throw a urllib2.HTTPError.
 
 	# N.B. urllib2 will follow any 302 redirects. Also, the "open" call above may throw a urllib2.HTTPError which is checked for below.
@@ -75,13 +75,13 @@ def http_req(url, post=None, headers={}):
 
 	return response.read()
 
-print 'Welcome to Garmin Connect Exporter!'
+print('Welcome to Garmin Connect Exporter!')
 
 # Create directory for data files.
 if isdir(args.directory):
-	print 'Warning: Output directory already exists. Will skip already-downloaded files and append to the CSV file.'
+	print('Warning: Output directory already exists. Will skip already-downloaded files and append to the CSV file.')
 
-username = args.username if args.username else raw_input('Username: ')
+username = args.username if args.username else input('Username: ')
 password = args.password if args.password else getpass()
 
 # Maximum number of activities you can request at once.  Set and enforced by Garmin.
@@ -91,8 +91,8 @@ limit_maximum = 100
 url_gc_login     = 'https://sso.garmin.com/sso/login?service=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&webhost=olaxpw-connect04&source=https%3A%2F%2Fconnect.garmin.com%2Fen-US%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_US&id=gauth-widget&cssUrl=https%3A%2F%2Fstatic.garmincdn.com%2Fcom.garmin.connect%2Fui%2Fcss%2Fgauth-custom-v1.1-min.css&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&usernameShown=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&generateExtraServiceTicket=false'
 url_gc_post_auth = 'https://connect.garmin.com/post-auth/login?'
 url_gc_search    = 'http://connect.garmin.com/proxy/activity-search-service-1.0/json/activities?'
-url_gc_gpx_activity = 'http://connect.garmin.com/proxy/activity-service-1.1/gpx/activity/'
-url_gc_tcx_activity = 'http://connect.garmin.com/proxy/activity-service-1.1/tcx/activity/'
+url_gc_gpx_activity = 'https://connect.garmin.com/modern/proxy/download-service/export/gpx/activity/'
+url_gc_tcx_activity = 'https://connect.garmin.com/modern/proxy/download-service/export/tcx/activity/'
 url_gc_original_activity = 'http://connect.garmin.com/proxy/download-service/files/activity/'
 
 # Initially, we need to get a valid session cookie, so we pull the login page.
@@ -153,7 +153,7 @@ while total_downloaded < total_to_download:
 	search_params = {'start': total_downloaded, 'limit': num_to_download}
 	# Query Garmin Connect
 	result = http_req(url_gc_search + urlencode(search_params))
-	json_results = json.loads(result)  # TODO: Catch possible exceptions here.
+	json_results = json.loads(result.decode('utf-8'))  # TODO: Catch possible exceptions here.
 		
 
 	search = json_results['results']['search']
@@ -170,67 +170,64 @@ while total_downloaded < total_to_download:
 	# Process each activity.
 	for a in activities:
 		# Display which entry we're working on.
-		print 'Garmin Connect activity: [' + a['activity']['activityId'] + ']',
-		print a['activity']['activityName']['value']
-		print '\t' + a['activity']['beginTimestamp']['display'] + ',',
+		print('Garmin Connect activity: [' + a['activity']['activityId'] + ']', end=' ')
+		print(a['activity']['activityName']['value'])
+		print('\t' + a['activity']['beginTimestamp']['display'] + ',', end=' ')
 		if 'sumElapsedDuration' in a['activity']:
-			print a['activity']['sumElapsedDuration']['display'] + ',',
+			print(a['activity']['sumElapsedDuration']['display'] + ',', end=' ')
 		else:
-			print '??:??:??,',
+			print('??:??:??,', end=' ')
 		if 'sumDistance' in a['activity']:
-			print a['activity']['sumDistance']['withUnit']
+			print(a['activity']['sumDistance']['withUnit'])
 		else:
-			print '0.00 Miles'
+			print('0.00 Miles')
 
 		if args.format == 'gpx':
 			data_filename = args.directory + '/activity_' + a['activity']['activityId'] + '.gpx'
 			download_url = url_gc_gpx_activity + a['activity']['activityId'] + '?full=true'
-			file_mode = 'w'
 		elif args.format == 'tcx':
 			data_filename = args.directory + '/activity_' + a['activity']['activityId'] + '.tcx'
 			download_url = url_gc_tcx_activity + a['activity']['activityId'] + '?full=true'
-			file_mode = 'w'
 		elif args.format == 'original':
 			data_filename = args.directory + '/activity_' + a['activity']['activityId'] + '.zip'
 			fit_filename = args.directory + '/' + a['activity']['activityId'] + '.fit'
 			download_url = url_gc_original_activity + a['activity']['activityId']
-			file_mode = 'wb'
 		else:
 			raise Exception('Unrecognized format.')
 
 		if isfile(data_filename):
-			print '\tData file already exists; skipping...'
+			print('\tData file already exists; skipping...')
 			continue
 		if args.format == 'original' and isfile(fit_filename):  # Regardless of unzip setting, don't redownload if the ZIP or FIT file exists.
-			print '\tFIT data file already exists; skipping...'
+			print('\tFIT data file already exists; skipping...')
 			continue
 
 		# Download the data file from Garmin Connect.
 		# If the download fails (e.g., due to timeout), this script will die, but nothing
 		# will have been written to disk about this activity, so just running it again
 		# should pick up where it left off.
-		print '\tDownloading file...',
+		print('\tDownloading file...', end=' ')
 
 		try:
 			data = http_req(download_url)
-		except urllib2.HTTPError as e:
+		except urllib.error.HTTPError as e:
 			# Handle expected (though unfortunate) error codes; die on unexpected ones.
 			if e.code == 500 and args.format == 'tcx':
 				# Garmin will give an internal server error (HTTP 500) when downloading TCX files if the original was a manual GPX upload.
 				# Writing an empty file prevents this file from being redownloaded, similar to the way GPX files are saved even when there are no tracks.
 				# One could be generated here, but that's a bit much. Use the GPX format if you want actual data in every file,
 				# as I believe Garmin provides a GPX file for every activity.
-				print 'Writing empty file since Garmin did not generate a TCX file for this activity...',
-				data = ''
+				print('Writing empty file since Garmin did not generate a TCX file for this activity...', end=' ')
+				data = b''
 			elif e.code == 404 and args.format == 'original':
 				# For manual activities (i.e., entered in online without a file upload), there is no original file.
 				# Write an empty file to prevent redownloading it.
-				print 'Writing empty file since there was no original activity data...',
-				data = ''
+				print('Writing empty file since there was no original activity data...', end=' ')
+				data = b''
 			else:
 				raise Exception('Failed. Got an unexpected HTTP error (' + str(e.code) + ').')
 
-		save_file = open(data_filename, file_mode)
+		save_file = open(data_filename, 'wb')
 		save_file.write(data)
 		save_file.close()
 
@@ -282,7 +279,7 @@ while total_downloaded < total_to_download:
 		csv_record += empty_record if 'lossElevation' not in a['activity'] else '"' + a['activity']['lossElevation']['value'].replace('"', '""') + '"'
 		csv_record += '\n'
 
-		csv_file.write(csv_record.encode('utf8'))
+		csv_file.write(csv_record)
 
 		if args.format == 'gpx':
 			# Validate GPX data. If we have an activity without GPS data (e.g., running on a treadmill),
@@ -292,25 +289,26 @@ while total_downloaded < total_to_download:
 			gpx_data_exists = len(gpx.getElementsByTagName('trkpt')) > 0
 
 			if gpx_data_exists:
-				print 'Done. GPX data saved.'
+				print('Done. GPX data saved.')
 			else:
-				print 'Done. No track points found.'
+				print('Done. No track points found.')
 		elif args.format == 'original':
-			if args.unzip and data_filename[-3:].lower() == 'zip':  # Even manual upload of a GPX file is zipped, but we'll validate the extension.
-				print "Unzipping and removing original files...",
-				zip_file = open(data_filename, 'rb')
-				z = zipfile.ZipFile(zip_file)
-				for name in z.namelist():
-					z.extract(name, args.directory)
-				zip_file.close()
-				remove(data_filename)
-			print 'Done.'
+			if len(data) > 0:
+				if args.unzip and data_filename[-3:].lower() == 'zip':  # Even manual upload of a GPX file is zipped, but we'll validate the extension.
+					print("Unzipping and removing original files...", end=' ')
+					zip_file = open(data_filename, 'rb')
+					z = zipfile.ZipFile(zip_file)
+					for name in z.namelist():
+						z.extract(name, args.directory)
+					zip_file.close()
+					remove(data_filename)
+			print('Done.')
 		else:
 			# TODO: Consider validating other formats.
-			print 'Done.'
+			print('Done.')
 	total_downloaded += num_to_download
 # End while loop for multiple chunks.
 
 csv_file.close()
 
-print 'Done!'
+print('Done!')
